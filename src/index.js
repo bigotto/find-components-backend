f6ef61480605fee981ef7057f60dfd8d35f8a8a0
Bruno Bigotto <bbigotto@gmail.com>
2020-09-14 17:54:07 -0400

    Tratamento da consulta do banco e entregando o dado para o frontend

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const createUser = require('./utils/firebase/createUser');
 const login = require('./utils/firebase/login');
+const db = require('./database/connection');
 
-const port = 3000;
+const port = 3333;
 const app = express();
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.get('/login', async (req, res) => {
     const data = req.body;
@@ -18,6 +21,15 @@ app.post('/signup', async (req, res) => {
 
     const user = await createUser(data);
     return res.status(201).send(user);
+})
+
+app.get('/db', async (req, res) => {
+    const total = await db('components')
+        .select('name', 'number', 'position')
+        .join('gavetas', 'components.id', 'gavetas.component')
+        .where('name', 'ilike', '%CAPACITOR%');
+
+    return res.status(202).send(total);
 })
 
 app.listen(port, () => {
